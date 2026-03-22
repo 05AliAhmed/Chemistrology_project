c1b96b63f9aa343431b70740fd85d7d0090309cc
05AliAhmed <20ali0504@gmail.com>
2026-03-22 13:12:18 +0800

    Pause menu
    added pause menu. It is working.

--- Assets/Scripts/lvl1Buttons.cs
+++ Assets/Scripts/lvl1Buttons.cs
@@ -5,15 +5,17 @@ using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.SocialPlatforms;
 using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 public class lvl1Buttons : MonoBehaviour
 {
-    [SerializeField] int amendScore = 10;
-    int score;
+    [SerializeField] GameObject pausemenu; // reference to pausemenu panel
+    [SerializeField] int amendScore = 10; // in inspector can change the value
+    int score; // keeping the score variable.
     [SerializeField] TMP_Text scoreTxt;
     [SerializeField] TMP_Text highScoreTxt;
 
-    public void ScoreSystem()
+    public void ScoreSystem() // will late be connected with shooting system to count scores
     {
         score += amendScore;
         scoreTxt.text = score.ToString();
@@ -24,12 +26,24 @@ public class lvl1Buttons : MonoBehaviour
         }
         Debug.Log(score);
     }
-    public void BackOption()
+    public void BackOption() // will late be replaced with settings button
     {
         SceneManager.LoadScene(0);
     }
+
+    public void PauseButton() // is used for pasusing the game
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ResumeButton() // for resuming the game
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
     void Start()
     {
+        pausemenu.SetActive(false);
         highScoreTxt.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
     }
 

5e91a97b5ba325acb339c3bcb57e3ce4c0589b93
05AliAhmed <20ali0504@gmail.com>
2026-03-23 13:17:21 +0800

    LEvel menu
    Added level menu
    fact display system
    miss system

---
+++ Assets/Scripts/LMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LMenu : MonoBehaviour
+{
+
+    public void LevelOne() // takes to level 1
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/lvl1Buttons.cs
+++ Assets/Scripts/lvl1Buttons.cs
@@ -1,37 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEngine.SocialPlatforms;
-using UnityEngine.UIElements;
-using Unity.VisualScripting;
+using UnityEngine.Rendering;
+using UnityEngine.SocialPlatforms.Impl;
+
 
 public class lvl1Buttons : MonoBehaviour
 {
     [SerializeField] GameObject pausemenu; // reference to pausemenu panel
     [SerializeField] int amendScore = 10; // in inspector can change the value
-    int score; // keeping the score variable.
+    [SerializeField] int loseScore = 50; // in inspector can change the value
+    [SerializeField] int targetsHit; // mannualy hitting and checking checking if all targets been hit | Number of electron in atom
+    [SerializeField] int totalTarget; // number of electron is an atom per level
+    [SerializeField] int scoreToPassLOne; // testing purpose if score 500 proceed to level 2 if less display a msg after all targets been hit
     [SerializeField] TMP_Text scoreTxt;
     [SerializeField] TMP_Text highScoreTxt;
+    int score; // keeping the score variable.
+    int highScore;
 
-    public void ScoreSystem() // will late be connected with shooting system to count scores
+
+    public void ScoreSystem() // will late be connected with shooting system to count scores | Score Button
     {
         score += amendScore;
         scoreTxt.text = score.ToString();
+        highScore = score; // testing purpose not working remove highscore int and this line later.
         if(score > PlayerPrefs.GetInt("Highscore",0))
         {
             PlayerPrefs.SetInt("Highscore", score);
-            highScoreTxt.text = score.ToString();    
+            highScoreTxt.text = score.ToString();   
         }
         Debug.Log(score);
     }
-    public void BackOption() // will late be replaced with settings button
+
+    public void LoseScore()
+    {
+        score -= loseScore;
+        scoreTxt.text = score.ToString();
+    }
+    public void BackOption() // will late be replaced with settings button | bact to main menu button 
     {
         SceneManager.LoadScene(0);
     }
 
-    public void PauseButton() // is used for pasusing the game
+    public void PauseButton() // is used for pasusing the game | Settings button
     {
         pausemenu.SetActive(true);
         Time.timeScale = 0f;
@@ -41,6 +53,10 @@ public class lvl1Buttons : MonoBehaviour
         pausemenu.SetActive(false);
         Time.timeScale = 1f;
     }
+    public void LevelMenu() // to level menu
+    {
+        SceneManager.LoadScene(2);
+    }
     void Start()
     {
         pausemenu.SetActive(false);
@@ -49,6 +65,26 @@ public class lvl1Buttons : MonoBehaviour
 
     void Update()
     {
-        
+        if(targetsHit == totalTarget)
+        {
+            //pop fact screen, display score, high score | In proper play
+            if(score < scoreToPassLOne)
+            {
+                Debug.Log($"You need {scoreToPassLOne - score} more points to proceed");
+                Debug.Log($"{score}");
+                Debug.Log($"{highScore}");
+            }
+            else
+            {
+                Debug.Log("Here is the fact");
+                Debug.Log($"{score}");
+                Debug.Log($"{highScore}");
+                // display next button | next button.setactive(true);               
+            }
+        }
+        else
+        {
+            Debug.Log("Not yet");
+        }
     }
 }
